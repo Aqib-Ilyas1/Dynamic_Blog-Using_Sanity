7de1b43cd4df7075121c01cf2b3835ac4277e9a1
Aqib Ilyas <aqibilyas289@gmail.com>
2025-01-04 01:28:43 +0500

    .

--- my-app/src/sanity/env.ts
+++ my-app/src/sanity/env.ts
@@ -1,16 +1,16 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-01'
 
-  export const dataset =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "production"
+  // export const dataset =
+  // process.env.NEXT_PUBLIC_SANITY_API_VERSION || "production"
 
   // export const projectId =
   // process.env.NEXT_PUBLIC_SANITY_API_VERSION || "l9i9p73k"
 
-// export const dataset = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_DATASET,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-// )
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'l9i9p73k',
